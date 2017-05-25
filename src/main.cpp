61e18f4712cbe20649788d5bbcfb5ed8c6ea1cda
Nathan Smith <cule_dood@hotmail.com>
2017-05-25 17:52:10 -0600

    Added the obj directory which will not contain any files to be tracked. It's just to clean up the working directory structure. Also, added the namespace instead of explicitly calling std every time.

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,20 @@
 #include <string>
 #include "hero.h"
 
+using namespace std;
+
 int main()
 {
 	Hero todd("Todd", 12, 6); //Instantiate a new hero
 
 	Hero jeff("Jeff", 15, 12);
 
-	std::cout << "Our hero's name is " << todd.getName() << " with " << todd.getHealth() << " health.\n";
-	std::cout << jeff.getName() << " doesn't like " << todd.getName() << "\n";
-	std::cout << jeff.getName() << "'s strength is " << jeff.getStrength() << "\n";
+	cout << "Our hero's name is " << todd.getName() << " with " << todd.getHealth() << " health.\n";
+	cout << jeff.getName() << " doesn't like " << todd.getName() << "\n";
+	cout << jeff.getName() << "'s strength is " << jeff.getStrength() << "\n";
 	jeff.attack(todd); //Look out, Todd!
 	//todd.ouch(12); //TODO shouldn't need to do this, the attack should do it on it's own
-	std::cout << jeff.getName() << " has " << jeff.getHealth() << " remaining. Poor Jeff.\n";
-	std::cout << todd.getName() << " has " << todd.getHealth() << " remaining. Poor Tood.\n";
-	//std::cout << "Test of toString: " << *jeff
+	cout << jeff.getName() << " has " << jeff.getHealth() << " remaining. Poor Jeff.\n";
+	cout << todd.getName() << " has " << todd.getHealth() << " remaining. Poor Tood.\n";
+	//cout << "Test of toString: " << *jeff
 }
