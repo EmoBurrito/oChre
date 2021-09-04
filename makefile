fe8e8561a855fa7e9e154a0c0d1f308dc17d273a
EmoBurrito <xautotx@gmail.com>
2021-09-03 22:25:04 -0600

    Added platform and architecture detection. Version is now passed in via makefile.

--- makefile
+++ makefile
@@ -4,19 +4,20 @@ LINK=$(CC)
 ifeq ($(OS),Windows_NT)
 	extension=.exe
 	purge=del
+	extra_args=-static-libgcc -static-libstdc++
 else
 	purge=rm
 endif
 
 exec_main: creature party main
-	$(CC) -o bin/oChre$(extension) bin/main.o bin/party.o bin/creature.o -static-libgcc -static-libstdc++
+	$(CC) -o bin/oChre$(extension) bin/main.o bin/party.o bin/creature.o $(extra_args)
 exec_tests:  test_creature test_party
 	$(CC) -o bin/tests$(extension) bin/creature.o bin/tests_creature.o bin/party.o bin/tests_party.o
 
 creature:
 	$(CC) -o bin/creature.o -c src/creature.cpp
 main:
-	$(CC) -o bin/main.o -c src/main.cpp
+	$(CC) -o bin/main.o -c src/main.cpp -DOCHRE_VERSION=\"Change.Me\"
 party:
 	$(CC) -o bin/party.o -c src/party.cpp
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,37 @@
 #include "creature.hpp"
 #include "party.hpp"
 
+#if _WIN32 || _WIN64
+	#define OCHRE_PLATFORM "Windows"
+	#if _WIN64
+		#define OCHRE_ARCH "x86_64"
+	#elif _WIN32
+		#define OCHRE_ARCH "i386"
+	#endif
+#elif __GNUC__
+	#define OCHRE_PLATFORM "Linux"
+	#if __x86_64__
+		#define OCHRE_ARCH "x86_64"
+	#elif __aarch64__
+		#define OCHRE_ARCH "aarch64"
+	#endif
+#endif
+
+#ifndef OCHRE_VERSION
+	#define OCHRE_VERSION "UNDEFINED"
+#endif
+#ifndef OCHRE_PLATFORM
+	#define OCHRE_PLATFORM "UNKNOWN"
+#endif
+#ifndef OCHRE_ARCH
+	#define OCHRE_ARCH "UNKNOWN"
+#endif
+
 int main()
 {
-	std::string banner = "Ochrewood Tribute";
-	std::string banner_header = std::string(banner.length()+4, '=') + '\n';
-	std::cout << banner_header << "| " << banner << " |\n" << banner_header;
+	std::string banner = "| Ochrewood Tribute v" + std::string(OCHRE_VERSION) + " for " + std::string(OCHRE_PLATFORM) + " on " + std::string(OCHRE_ARCH) + " |\n";
+	std::string banner_header = std::string(banner.length()-1, '=') + '\n';
+	std::cout << banner_header << banner << banner_header;
 
 	Human human1;
 	Human human2("Todd");
